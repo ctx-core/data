cda5b2eb18771d49021b4a045dccf9bb4832277e
Brian Takita <brian.takita@gmail.com>
2020-07-28 01:59:06 -0700

    ∋ _a1_present

--- lib.ts
+++ lib.ts
@@ -1,6 +1,6 @@
 import { falsy } from '@ctx-core/function'
 import { I__ } from '@ctx-core/combinators'
-import { _present__a1 } from '@ctx-core/array'
+import { _a1_present } from '@ctx-core/array'
 export type h1__filter = { [key:string]:boolean }
 /**
  * Returns a h1__filter with the values filtered by `fn`.
@@ -28,4 +28,4 @@ export function _fn__h1__filter(fn = I__) {
  * Returns a h1__filter where the value is a
  * boolean of whether the array has items present
  */
-export const _h1__present__a1 = _fn__h1__filter(_present__a1)
+export const _h1__present__a1 = _fn__h1__filter(_a1_present)
