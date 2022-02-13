0abac1b6ab571c8a27bbd8eb5fee13b464284340
Brian Takita <brian.takita@gmail.com>
2022-02-12 23:27:22 -0600

    fix: filter_r_2: jsdoc

--- src/filter_r_2.ts
+++ src/filter_r_2.ts
@@ -2,16 +2,13 @@ import { I__ } from '@ctx-core/combinators'
 import { filter_r_ } from './filter_r_.js'
 import type { filter_r_T } from './filter_r_T.js'
 /**
- * Returns a function that calls _filter_h1 with fn
+ * Returns a function that calls filter_r_ with fn
  */
 export function filter_r_2<Obj extends object = object>(
 	fn:(val:Obj[keyof Obj], key:string, l0_r:Obj)=>any = I__
 ):((l1_r:Obj|undefined)=>filter_r_T|undefined) {
 	return (l1_r:Obj|undefined)=>filter_r_<Obj>(l1_r, fn)
 }
-// export const a_present__h1 = _filter_h1_fn(
-// 	(val, key, h1)=>a_present_()
-// )
 export {
 	filter_r_2 as filter_h_2,
 	filter_r_2 as _fn__h1__filter,
